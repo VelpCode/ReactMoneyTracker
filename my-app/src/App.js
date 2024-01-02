e578b047a1f99e41ca3cf7d373059edd2f1f5ca3
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-02 17:18:14 -0500

    creating layout of app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,10 +21,31 @@ function App() {
         <div className = "transaction">
           <div className = "left">
             <div className = "name">New Samsung TV</div>
+            <div className='description'>it was time for a new tv</div>
           </div>
           <div className = "right">
             <div className = "price">$500</div>
-            <div className = "datetime">2022-12-18 15:45</div>
+            <div className = "datetime">2022-12-18 15:45  </div>
+          </div>
+        </div>
+        <div className = "transaction">
+          <div className = "left">
+            <div className = "name">New Samsung TV</div>
+            <div className='description'>it was time for a new tv</div>
+          </div>
+          <div className = "right">
+            <div className = "price">$500</div>
+            <div className = "datetime">2022-12-18 15:45  </div>
+          </div>
+        </div>
+        <div className = "transaction">
+          <div className = "left">
+            <div className = "name">New Samsung TV</div>
+            <div className='description'>it was time for a new tv</div>
+          </div>
+          <div className = "right">
+            <div className = "price">$500</div>
+            <div className = "datetime">2022-12-18 15:45  </div>
           </div>
         </div>
       </div>
